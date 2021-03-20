2fb1d32c4726c89411e7cfdb6a80bbbf081e0f63
HITsz-yuanhao <75562947+HITsz-yuanhao@users.noreply.github.com>
2021-03-20 23:56:19 +0800

    Add files via upload

--- var/var.py
+++ var/var.py
@@ -1,20 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 20 18:58:47 2021
+
+@author: yh
+"""
+
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 """
 Created on Wed Feb 24 22:29:42 2021
-
 @author: yh
 """
 
 import h5py
 import numpy as np
-from pathlib import Path
 import argparse as ap
 
 class readHDF(object):
-    def __init__(self, filename, path):
-        self.path = Path(path)
-        self.filename = self.path/filename
+    def __init__(self, filename):
+        self.filename = filename
         
     def readInfo(self, var):
         f = h5py.File(self.filename, 'r')
@@ -27,7 +32,12 @@ class readHDF(object):
                 print('The number of the variable is', f[svar][0])
             print('----------------------------')
 
-        
+    def printAll(self):
+        f = h5py.File(self.filename, 'r')
+        for key in f.keys():
+            print(key)
+
+       
 #HDF5的读取：
 # f = h5py.File('avg_recy.h5','r')   #打开h5文件
 # 可以查看特定的变量
@@ -42,15 +52,9 @@ class readHDF(object):
 #for key in f.keys():
 #     print(f[key].name)
 #     print(f[key].shape)
+
 if __name__ == '__main__':
     parser = ap.ArgumentParser(prog='HDFView', description='Generate a xdmf file to view HDF file.')
-    parser.add_argument(
-                        '-p', '--path',                # Option Name
-                        required='False',               # Requirement
-                        help='Type the Path',          # Help log
-                        metavar='Path',                # [-p path]
-                        default='.'
-                        )
     parser.add_argument(
                         '-f', '--filename',            # Option Name
                         required='True',               # Requirement
@@ -64,6 +68,17 @@ if __name__ == '__main__':
                         help='Type the Variables(must)',
                         metavar='Variables'
                         )
+    parser.add_argument(
+                        '-a', '--all',
+                        action='store_true',
+                        help='Type nothing'
+                        )
     args = parser.parse_args()
-    a = readHDF(args.filename, args.path).readInfo(args.variables)
+    #a = readHDF(args.filename).readInfo(args.variables)
+    a = readHDF(args.filename)
+    if args.variables is not None:
+        a.readInfo(args.variables)
+    elif args.all:
+        a.printAll()
 
+    
